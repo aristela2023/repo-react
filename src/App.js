cc063f3260c2e33220bb957ff209f4c5cdac385a
Aristela <aristela1@gmail.com>
2023-09-24 13:19:02 -0300

    Mi primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Menu from './Menu'
+
+function App() {
+  return (
+    <div className="container">
+        <Menu/>
+  Hola!
+    </div>
+  );
+}
+
+export default App;
