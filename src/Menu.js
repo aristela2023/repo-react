fa378f5e95e59ff779cc55155806e6adcc203da0
Aristela <aristela1@gmail.com>
2023-09-26 16:27:52 -0300

    mi primer commit

--- src/Menu.js
+++ src/Menu.js
@@ -6,7 +6,7 @@ render(){
     return(
         <div className="row">
             <div className="col-12 d-flex align- items- center">
-                <h1> Artio </h1>
+                <h1> Artio REVISADO</h1>
                 <span className="mx-5"> Inicio</span>
                 <span className="mx-5"> Artista</span>
                 <span className="mx-5"> Contacto</span>
