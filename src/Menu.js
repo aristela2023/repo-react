cf71d24005ef94ae88713b9599964c9b19cd7efa
Aristela <aristela1@gmail.com>
2023-10-10 16:54:22 -0300

    actualizacion

--- src/Menu.js
+++ src/Menu.js
@@ -6,7 +6,7 @@ render(){
     return(
         <div className="row bg-light fixed-top px-4 py-2">
             <div className="col-12 d-flex align- items- center bg-light fixed-top py-2 px-4">
-                <h1> Artio REVISADO</h1>
+                <h1> Artio actualizado</h1>
                 <span className="mx-3"> Inicio</span>
                 <span className="mx-3"> Artista</span>
                 <span className="mx-3"> Contacto</span>
